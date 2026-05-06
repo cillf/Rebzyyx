347f28dfe0ac5a209cdd5c764a7d64458ab988dc
cillf <145763036+cillf@users.noreply.github.com>
2026-05-07 09:44:47 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ window.addEventListener("DOMContentLoaded", () => {
   container.textContent = "";
   
   let index = 0;
-  const speed = 40;
+  let currentSpeed = 20;
+  let fastMode = false;
+
+  setTimeout(() => {
+    fastMode = true;
+  }, 500);
 
   function type() {
     if (index < fullText.length) {
       container.textContent += fullText.charAt(index);
+
+      let delay = fastMode ? 3 : currentSpeed;
+
       index++;
-      setTimeout(type, speed);
+      setTimeout(type, delay);
     }
   }
+
   type();
 });
